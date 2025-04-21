9e5f3e63675a345ac1916d775d2541ee8d98d062
Ivan Cañete <caneteivan96@gmail.com>
2025-04-21 16:31:47 -0300

    Primer commit para js

---
+++ main.js
@@ -0,0 +1,72 @@
+/* let nombreUno = 'Ivan';
+let booleanVerdadero = true;
+let booleanFalso = false;
+let numero = 10;
+
+nombreUno = 'Ivan';
+console.log(nombreUno);
+
+const anio = 2025;
+console.log(typeof anio, typeof nombreUno); */
+
+
+/* 
+let n1 = 10
+let n2 = 20
+let resultado = n1 + n2
+const PI = 3.14
+let resultado = n2 * PI
+
+console.log(resultado);
+ */
+
+
+/* let pregunta = prompt('decime tu nombre');
+let edad = parseInt(prompt('decime tu edad'));
+console.log(pregunta);
+console.log(typeof edad);
+
+let edadDos = Number(prompt('decime tu edad2'));
+console.log(typeof edadDos); */
+/* 
+let pregunta = prompt('decime tu nombre')
+ */
+
+
+/* let n1 = 100;
+let n2 = 200;
+let resultado = n1 + n2;
+
+alert(resultado);
+
+let pregunta = ('¿Confirma la operacion?');
+
+confirm(pregunta); */
+
+// Ejemplo 1 - condicionales
+
+/* let edad = 21;
+
+if(edad < 18){
+    console.log('Usted es menor de edad');
+}
+if(edad === 19){
+    console.log('Usted es mayor de edad');
+}
+else{
+    console.log('No sabemos su edad');
+} */
+
+/* let temperatura = 3;
+
+if(temperatura > 28){
+    console.log('Parece que volvió el verano');
+}
+else if ( Temperatura < 15){
+    console.log('Dia fresco');
+}
+else{
+    console.log('No sabemos la temperatura');
+} */
+
+
